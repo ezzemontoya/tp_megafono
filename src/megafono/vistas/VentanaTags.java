9b7c7201f5469d1324f9b647b16f8083fc159d7e
Ezequiel <ezzemontoya@outlook.com>
2017-06-02 18:20:22 -0300

    Se sube el tag con arrayList

--- src/megafono/vistas/VentanaTags.java
+++ src/megafono/vistas/VentanaTags.java
@@ -4,27 +4,32 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.TextField;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Tree;
 
 import megafono.domain.model.Tag;
 import megafono.services.TagServices;
 
-public class VentanaTags extends VerticalLayout implements View{
+public class VentanaTags extends HorizontalLayout implements View{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	protected static final String NAME = "";
-	
+		
 	private TagServices tagService = TagServices.getTagService();
 	
 	public VentanaTags(){
 		setSizeFull();
+		
+		final Auxiliar auxi = new Auxiliar(); 
+		
+		final Tree arbol = new Tree("Tags");
+		addComponents(arbol);
 		
 		final TextField tag = new TextField("Ingrese el nuevo tag");
 		addComponent(tag);
@@ -44,10 +49,12 @@ public class VentanaTags extends VerticalLayout implements View{
 				String tagAux = tag.getValue();
 				System.out.println(tag.getValue());
 				Tag aux = new Tag(tag.getValue());
-				System.out.println(aux.toString());
-				tagService.guardar(aux);
-				System.out.println("Guardado");
-				Notification.show("Tag "+ tagAux + "guardado", Type.TRAY_NOTIFICATION);
+				//TODO - arreglar esto!!!!!
+//				tagService.guardar(aux);
+				auxi.misTags.add(aux);
+				arbol.addItem(tagAux);
+				Notification.show("Tag "+ tagAux + " guardado", Type.TRAY_NOTIFICATION);
+				
 			}
 		});
 		botones.addComponent(guardarTag);
