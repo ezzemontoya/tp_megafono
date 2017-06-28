64a418ec95953aaf0e5deed234cda2be739b38e2
Ezequiel <ezzemontoya@outlook.com>
2017-06-27 22:52:50 -0300

    se actualiza repo

--- src/megafono/vistas/VentanaPrincipal.java
+++ src/megafono/vistas/VentanaPrincipal.java
@@ -1,40 +1,29 @@
 package megafono.vistas;
 
-import javax.servlet.annotation.WebServlet;
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.HorizontalLayout;
 
-import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.navigator.Navigator;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+public class VentanaPrincipal extends HorizontalLayout implements View {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected static final String NAME = "";
 
 
-@SuppressWarnings("serial")
-@Theme("valo")
-public class VentanaPrincipal extends UI {
+	public VentanaPrincipal() {
+		setSizeFull();
+		
+		
 
-	@WebServlet(value = "/*", asyncSupported = true)
-	@VaadinServletConfiguration(productionMode = false, ui = VentanaPrincipal.class)
-	public static class Servlet extends VaadinServlet {
 	}
-
 
 	@Override
-	protected void init(VaadinRequest request) {
-		final VerticalLayout layout = new VerticalLayout();
-		layout.setMargin(true);
-		setContent(layout);
-		
-		Navigator navigator;
-		navigator = new Navigator(this, this);
-		// Y creamos y registramos las views (pantallas)
-		navigator.addView(VentanaTagsAgregar.NAME, new VentanaTagsAgregar());
-		navigator.addView(VentanaTagsBorrar.NAME, new VentanaTagsBorrar());
-		
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
 
 	}
-	
 
 }
